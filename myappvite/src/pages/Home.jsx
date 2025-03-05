0244d8c2ed1ee885a87ced72f7bca258629d7fd1
abhiram <abhirammotupalli.141@gmail.com>
2025-03-05 14:23:26 +0530

    set

---
+++ myappvite/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+const Home = () => {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>ABHI RAM</h1>
+      <p>9994449990</p>
+    </div>
+  );
+};
+
+export default Home;
